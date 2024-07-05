c5cbbb2f5453b7b6ed965854c759825c7aa11880
Rafael Borges <rafaelsfb@gmail.com>
2024-07-05 20:47:20 -0300

    Update install.sh

--- openvpn/install.sh
+++ openvpn/install.sh
@@ -1,22 +1,22 @@
 echo "----> Install OpenVPN Application Server"
 
-echo "\n\n"
+echo
 echo "--> Docker Install <--"
 sleep 2
-echo "\n\n"
+echo
 apt install docker.io -y
 
 sleep 2
 echo "--> Pull image OpenVPN AS <--"
 sleep 2
-echo "\n\n"
+echo
 docker pull openvpn/openvpn-as
 
 
 sleep 2
 echo "--> Create container OpenVPN AS <--"
 sleep 2
-echo "\n\n"
+echo
 docker run -d \
   --name=openvpn-as --cap-add=NET_ADMIN \
   -p 943:943 -p 443:443 -p 1194:1194/udp \
@@ -27,5 +27,5 @@ docker run -d \
 sleep 2
 echo "--> Show Password OpenVPN AS <--"
 sleep 2
-echo "\n\n"
+echo
 docker logs openvpn-as | grep "Auto-generated pass"
