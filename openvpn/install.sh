b8b25baded25f5a4310b2ec511df7e1f2973b8a9
Rafael Borges <rafaelsfb@gmail.com>
2024-07-05 22:19:37 -0300

    Update install.sh

--- openvpn/install.sh
+++ openvpn/install.sh
@@ -24,8 +24,32 @@ docker run -d \
   openvpn/openvpn-as
 
 
-sleep 2
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # Sem cor
+
 echo "--> Show Password OpenVPN AS <--"
 sleep 2
+
+total=60
+
+echo "--> Wait OpenVPN SA Installation <--"
 echo
-docker logs openvpn-as | grep "Auto-generated pass"
+for ((i=1; i<=total; i++)); do
+    percentage=$((i * 100 / total))
+    
+    echo -ne "\r${GREEN}Process... ["
+    for ((j=0; j<i; j++)); do
+        echo -n "#"
+    done
+    for ((j=i; j<total; j++)); do
+        echo -n " "
+    done
+    echo -ne "] ${RED}$percentage%${NC}"
+    
+    sleep 1
+done
+echo -e "\n${NC}"
+
+PASSWORD=$(docker logs openvpn-as 2>/dev/null | grep "Auto-generated pass" | tail -n 1 | sed -n 's/.*Auto-generated pass = "\([^"]*\)".*/\1/p')
+echo "Password: $PASSWORD"
